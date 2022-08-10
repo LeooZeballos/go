c9577b878985255e0b857303b201e6e5df0d025f
LeooZeballos <zeballosleonel3@gmail.com>
2022-08-10 19:11:54 -0300

    tarea if

--- src/condicionales.go
+++ src/condicionales.go
@@ -34,4 +34,23 @@ func main() {
 	}
 	fmt.Println("Value:", value)
 
+	if isPair(6) {
+		fmt.Println("Number is pair")
+	} else {
+		fmt.Println("Number is odd")
+	}
+	if isValidUser("Alpha5", "MyPassword") {
+		fmt.Println("Credentials are valid")
+	} else {
+		fmt.Println("Credentials aren't valid")
+	}
+
+}
+
+func isPair(num int) bool {
+	return num%2 == 0
+}
+
+func isValidUser(userName, pass string) bool {
+	return userName == "Alpha" && pass == "MyPassword"
 }
